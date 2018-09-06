f21fe1facef26d54b5fce87d98630ca23dd013de
VoxWave <victor_bankowski@hotmail.com>
2018-09-06 18:33:45 +0300

    Breweryn kanssa leikkimistä

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_09_06_150408) do
+
+  create_table "breweries", force: :cascade do |t|
+    t.string "name"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
